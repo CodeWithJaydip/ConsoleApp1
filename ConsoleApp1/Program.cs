66bcb3f30f7855ff8b917725224f05d20ae8b162
Jaydip <59694720+CodeWithJaydip@users.noreply.github.com>
2021-08-17 18:19:38 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>();
+        list.Add(new KeyValuePair<string, string>("Name1", "Phone Num1"));
+        list.Add(new KeyValuePair<string, string>("Name1", "Phone Num2"));
+
+        foreach (KeyValuePair<string, string> item in list)
+        {
+            Console.Write(item.Key + "=>" + item.Value);
+        }
+
+        Console.ReadKey();
+    }
+}
